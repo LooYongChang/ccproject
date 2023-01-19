3a813acf573572cdc3d740678d755abe3d47cddf
Loo Yong Chang <90229430+LooYongChang@users.noreply.github.com>
2023-01-19 13:04:04 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$connect = mysqli_connect("localhost","root","","FYP");
+$connect = mysqli_connect("project.cp1vqrieerhc.us-east-1.rds.amazonaws.com","admin","admin123456","fyp");
 date_default_timezone_set("Asia/Kuala_Lumpur");
 /*
 if($connect)
